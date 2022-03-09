0f92afeb9f08de8cb6e15a1813f9fa2a9f2b22b5
Taz Uddin <tazuddinleton@gmail.com>
2022-03-09 07:52:35 +0600

    update variables

--- 003_variables.sh
+++ 003_variables.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-
-echo "MYVAR is: $MYVAR"
+# It's not required to declare variable prior to use it
+# Un declared uninitialized variables have default value of
+# empty string
+echo "\$MYVAR is: $MYVAR"
+if [$MYVAR eq ""]
+then 
+    echo "\$MYVAR is empty string"
+else
+    echo "\$MYVAR var is not empty string"
+fi
 MYVAR="hi there "
-echo "MYVAR is: $MYVAR"
+echo "\$MYVAR is: $MYVAR"
