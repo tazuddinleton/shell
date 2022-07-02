d973238496a4fd4f16c41007c03f4c9fb0fd3c0b
Taz Uddin <tazuddinleton@gmail.com>
2022-07-03 01:16:37 +0600

    update

--- 011_positional_parameters.sh
+++ 011_positional_parameters.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-echo $0
+# echo $0 $1 $2 $3
+# echo $@ # $@ is a special variable that retrieves all user input from $0 to all the way to the end
+
+# for P in $@
+# do 
+#     echo $P
+# done
+
+
+if [ -f "/etc/shadow" ] 
+then 
+    echo "shadow pass are enabled"
+    if [ -w "/etc/shadow" ]
+    then echo "you have permission to edit /etc/shadow"    
+    else echo "you do not have permission to edit /etc/shadow"
+    fi
+fi
+
+
+
